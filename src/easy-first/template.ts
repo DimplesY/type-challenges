bd4a622cf3139456e3fd0accb1b826a7114328d6
Dimples_YJ <2890841438@qq.com>
2022-07-31 16:36:11 +0800

    chore: js implemented

--- src/easy-first/template.ts
+++ src/easy-first/template.ts
@@ -2,5 +2,5 @@ type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
 
 
 function first(arr: any[]) {
-  return arr.length === 0 ? undefined : arr[0]
+  return arr.length === 0 ? 'never' : arr[0]
 }
