a130f41013d0474dfd1fcc53a31569a8178a57c7
Dimples_YJ <2890841438@qq.com>
2022-08-23 02:10:28 +0800

    chore: add MyParameters

---
+++ src/easy-parameters/test-cases.ts
@@ -0,0 +1,7 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const foo = (_arg1: string, _arg2: number): void => {}
+const bar = (_arg1: boolean, _arg2: { a: 'A' }): void => {}
+const baz = (): void => {}
+
+type cases = [Expect<Equal<MyParameters<typeof foo>, [string, number]>>, Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>, Expect<Equal<MyParameters<typeof baz>, []>>]
